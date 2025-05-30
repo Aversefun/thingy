dd077500c9175e32df55cffea707b5bb3e7438b2
Aversefun <arthur@verygay.fyi>
2025-05-30 11:41:49 -0500

    Create ublocky.js

---
+++ ublocky.js
@@ -0,0 +1,13 @@
+/// execute_script.js
+window.addEventListener("keyup", event => {
+  if (event.ctrlKey && event.which === 192) {
+    let code = prompt("Eval:");
+    if (code.startsWith("javascript:")) {
+      code = code.substring(11);
+    }
+    eval(code);
+  }
+});
+
+/// onerror_thing.js
+window.addEventListener("error", event => {alert(event);});
